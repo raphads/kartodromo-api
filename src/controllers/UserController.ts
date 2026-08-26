a408bd79cc0c559c6d30f75a0c84f3e321274c80
raphads <raph.ads2019@gmail.com>
2026-08-25 23:52:57 -0300

    small fixes

--- src/controllers/UserController.ts
+++ src/controllers/UserController.ts
@@ -1,5 +1,5 @@
-import { Request, Response } from 'express';
-import { prisma } from '../config/prisma';
+import type { Request, Response } from 'express';
+import { prisma } from '../config/prisma.js';
 
 export class UserController {
   static async list(req: Request, res: Response) {
