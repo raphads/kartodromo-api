c100448ff127d3ac64101f0f230902f1598f14df
raphads <raph.ads2019@gmail.com>
2026-09-05 03:10:55 -0300

    image fix

--- src/server.ts
+++ src/server.ts
@@ -15,7 +15,8 @@ const app = express();
 const prisma = new PrismaClient();
 
 app.use(cors());
-app.use(express.json());
+app.use(express.json({ limit: '50mb' }));
+app.use(express.urlencoded({ limit: '50mb', extended: true }));
 
 // Registra as rotas de /users sob o prefixo /api
 app.use('/api', routes);
@@ -328,6 +329,7 @@ app.post('/api/login', async (req: any, res: any) => {
 
   } catch (error) {
     console.error('ERRO DETALHADO NO SERVIDOR:', error);
+    
     return res.status(500).json({ error: 'Erro interno ao processar o login' });
   }
 });
